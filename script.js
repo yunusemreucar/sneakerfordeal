a188d83e596332146325f71d3231c3e9c53ad627
yunusemre <yunusemreucar8@gmail.com>
2025-05-31 18:19:23 +0300

    İlk commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+const div = document.querySelector('.section-panel1')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
